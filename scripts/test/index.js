092db8b4a3de5b412bc735c5f6357f94bea4fb92
陆沉 <surgesoft@gmail.com>
2024-01-30 17:25:46 +0800

    feat: support mysql_version matrix (#3)
    * Update node-test-mysql.yml
    
    feat: support mysql_version matrix
    
    * matrxi add mysql_version
    
    * fix: mysql_version
    
    * resolve output

--- scripts/test/index.js
+++ scripts/test/index.js
@@ -2,8 +2,10 @@ const core = require('@actions/core');
 
 const os = core.getInput('os').split(',').map(x => x.trim());
 const version = core.getInput('version').split(',').map(x => x.trim());
+const mysqlVersion = core.getInput('mysql_version').split(',').map(x => x.trim());
 
 core.setOutput('os', JSON.stringify(os));
 core.setOutput('version', JSON.stringify(version));
+core.setOutput('mysql_version', JSON.stringify(mysqlVersion));
 
-core.info(`os: ${os}, version: ${version}`);
+core.info(`os: ${os}, version: ${version}, mysql_version: ${mysqlVersion}`);
