b5742b05155affabc09c3f00598f269eb1c74f2e
raoha.rh <raohai.rh@antgroup.com>
2024-01-30 18:12:25 +0800

    fix: mysqlVersion default value

--- scripts/test/index.js
+++ scripts/test/index.js
@@ -2,7 +2,8 @@ const core = require('@actions/core');
 
 const os = core.getInput('os').split(',').map(x => x.trim());
 const version = core.getInput('version').split(',').map(x => x.trim());
-const mysqlVersion = core.getInput('mysql_version').split(',').map(x => x.trim());
+const mysqlVersionInput = core.getInput('mysql_version') || '5';
+const mysqlVersion = mysqlVersionInput.split(',').map(x => x.trim());
 
 core.setOutput('os', JSON.stringify(os));
 core.setOutput('version', JSON.stringify(version));
