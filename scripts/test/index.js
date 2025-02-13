47b0049b4cbe0586ef963de2c063d1443cddc7c2
fengmk2 <fengmk2@gmail.com>
2025-02-13 19:55:31 +0800

    f

--- scripts/test/index.js
+++ scripts/test/index.js
@@ -5,10 +5,6 @@ const version = core.getInput('version').split(',').map(x => x.trim());
 const mysqlVersionInput = core.getInput('mysql_version') || '5';
 const mysqlVersion = mysqlVersionInput.split(',').map(x => x.trim());
 
-core.setOutput('os', JSON.stringify(os));
-core.setOutput('version', JSON.stringify(version));
-core.setOutput('mysql_version', JSON.stringify(mysqlVersion === '' ? [] : mysqlVersion));
-
 const parallel = parseInt(core.getInput('parallel')) || 3;
 const node_index = [];
 const total_nodes = parallel;
@@ -16,7 +12,10 @@ for (let i = 0; i < total_nodes; i++) {
   node_index.push(i);
 }
 
+core.setOutput('os', JSON.stringify(os));
+core.setOutput('version', JSON.stringify(version));
+core.setOutput('mysql_version', JSON.stringify(mysqlVersion === '' ? [] : mysqlVersion));
 core.setOutput('node_index', JSON.stringify(node_index));
 core.setOutput('total_nodes', JSON.stringify(total_nodes));
 
-core.info(`os: ${os}, version: ${version}, mysql_version: ${mysqlVersion}, parallel: ${parallel}, node_index: ${node_index}, total_nodes: ${total_nodes}`);
+core.info(`[debug] os: ${JSON.stringify(os)}, version: ${JSON.stringify(version)}, mysql_version: ${JSON.stringify(mysqlVersion)}, parallel: ${parallel}, node_index: ${JSON.stringify(node_index)}, total_nodes: ${JSON.stringify(total_nodes)}`);
